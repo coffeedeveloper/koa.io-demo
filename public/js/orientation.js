9ca3c2d90198739eed41e70774c998e7bb0b2f7c
CoffeeDeveloper <coffeedeveloper@gmail.com>
2015-06-29 23:04:46 +0800

    connected

--- public/js/orientation.js
+++ public/js/orientation.js
@@ -1,4 +1,28 @@
+import Qs from 'qs';
+import cup from 'cupjs';
+
 $(function() {
+
+  var roomName = cup.guid(true);
+
+  console.log(roomName);
+
+  const querystring = location.search && location.search.substr(1);
+  var urlParams = {};
+
+  if (querystring) {
+    urlParams = Qs.parse(querystring);
+    console.log(urlParams);
+    if (urlParams.roomName) {
+      roomName = urlParams.roomName;
+    }
+  }
+
+  console.log(roomName);
+
+  var socket = io();
+
+  socket.emit('join', roomName);
   var $log = $('#log');
   var $sys = $log.find('.sys');
   var $info = $log.find('.info');
@@ -9,40 +33,75 @@ $(function() {
     y: $(window).height()
   };
 
-  $sys.html(`
-    window width: ${win.x}<br />
-    window height: ${win.y}
-  `);
+  var isdebug = false;
+  if (isdebug) {
+    $sys.html(`
+      window width: ${win.x}<br />
+      window height: ${win.y}
+    `);
+  }
 
-  if (window.DeviceOrientationEvent) {
-    $(window).on('deviceorientation', function(event) {
-      var e = event.originalEvent;
-      $info.html(`
-        gamma: ${e.gamma}<br />
-        beta: ${e.beta}<br />
-        alpha: ${e.alpha}
-      `);
 
-      var lr = e.gamma < 0 ? 'left' : 'right';
-      var fb = e.beta < 0 ? 'front' : 'back';
+  var move = (e) => {
+    var lr = e.gamma < 0 ? 'left' : 'right';
+    var fb = e.beta < 0 ? 'front' : 'back';
 
-      var pfb = Math.abs(e.beta) / 45;
-      if (pfb > 1) pfb = 1;
+    var pfb = Math.abs(e.beta) / 45;
+    if (pfb > 1) pfb = 1;
 
-      var plr = Math.abs(e.gamma) / 45;
-      if (plr > 1) plr = 1;
+    var plr = Math.abs(e.gamma) / 45;
+    if (plr > 1) plr = 1;
 
-      var maxx = (win.x * 0.5) - 40;
-      var maxy = (win.y * 0.5) - 40;
+    var maxx = (win.x * 0.5) - 40;
+    var maxy = (win.y * 0.5) - 40;
 
-      var x = (maxx * plr) * (lr == 'left' ? -1 : 1);
-      var y = (maxy * pfb) * (fb == 'front' ? -1 : 1);
+    var x = (maxx * plr) * (lr == 'left' ? -1 : 1);
+    var y = (maxy * pfb) * (fb == 'front' ? -1 : 1);
 
-      var prop = 'translate(' + x + 'px, ' + y + 'px)';
-      console.log(prop);
+    var prop = 'translate(' + x + 'px, ' + y + 'px)';
+    console.log(prop);
+    return prop;
+  };
 
-      $ball.css('transform', prop);
 
+  if (cup.is.mobile()) {
+    $('#mask').hide();
+    if (window.DeviceOrientationEvent) {
+      $(window).on('deviceorientation', function(event) {
+        var e = event.originalEvent;
+
+        if (isdebug) {
+          $info.html(`
+            gamma: ${e.gamma}<br />
+            beta: ${e.beta}<br />
+            alpha: ${e.alpha}
+          `);
+        }
+        
+        var o = {
+          gamma: e.gamma,
+          beta: e.beta,
+          alpha: e.alpha
+        };
+
+        $ball.css('transform', move(o));
+        socket.emit('order', roomName, o);
+      });
+    }
+  } else {
+    socket.on('joined', function(msg) {
+      $('#mask').fadeOut();
+    });
+
+    var qrurl = `${location.protocol}//${location.host}${location.pathname}?roomName=${roomName}`;
+    console.log(qrurl);
+    $('#qrcode').qrcode({
+      text: qrurl,
+      size: 200
+    });
+
+    socket.on('order', (e) => {
+      $ball.css('transform', move(e));
     });
   }
 });
